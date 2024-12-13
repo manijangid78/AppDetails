d82ba70a56b31a579ed2b031cec20a4296b1a3f1
Manish Kumar Jangid <manijangid78@gmail.com>
2024-12-13 23:05:14 +0530

    Initial commit 222

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+class Article(BaseModel):
+    id: int
+    name: str
+    price: float
+
+articles = []
 app = FastAPI()
 
 @app.get("/login")
@@ -9,6 +17,44 @@ async def home():
         "framework":"FastApi"
     }
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/articles", response_model=List[Article])
+async def read_articles():
+    return articles
+
+@app.post("/articles", response_model=Article)
+async def create_article(article: Article):
+    articles.append(article)
+    return article
+
+@app.put("/articles/{article_id}", response_model=Article)
+async def update_article(article_id: int, article: Article):
+    articles[article_id] = article
+    return article
+
+@app.delete("/articles/{article_id}")
+async def delete_article(article_id: int):
+    del articles[article_id]
+    return {"message": "Article deleted"}
+
+# @asyncpp.get("/articles", response_model=List[Article])
+# async def read_articles():
+#     return articles
+#
+# @app.post("/articles", response_model=Article)
+# async def create_article(article: Article):
+#     articles.append(article)
+#     return article
+#
+# @app.put("/articles/{article_id}", response_model=Article)
+# async def update_article(article_id:int, article:Article):
+#     articles[article_id] = article
+#     return article
+#
+# @app.delete("/article/{article_id}")
+# async def deleteArticle(article_id:int):
+#     del articles[article_id]
+#     return {"message": "Article deleted"}
+#
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
